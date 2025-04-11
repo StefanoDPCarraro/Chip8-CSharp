b4617592d298256592d69f737d98286a08616e80
Stefano <stefanodpc29@gmail.com>
2025-04-11 03:14:22 -0300

    Added SDL rendering and event listener for quit and keyboard, aswell as saving the pressed key to the keyoard variable

--- Chip8/Program.cs
+++ Chip8/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information]
-Console.WriteLine("Test");
+﻿using Chip8_CSharp.Chip8;
+using SDL2;
+
+public class Program{
+    static void Main(){
+        bool open = false;
+
+        ProcessingUnit cpu = new ProcessingUnit();
+
+        SDL.SDL_Init(SDL.SDL_INIT_EVERYTHING);
+        IntPtr window = SDL.SDL_CreateWindow("CHIP8", 100, 100, 640, 480, SDL.SDL_WindowFlags.SDL_WINDOW_RESIZABLE);
+        IntPtr nomequetuquiser = SDL.SDL_CreateRenderer(window,-1, SDL.SDL_RendererFlags.SDL_RENDERER_ACCELERATED);
+
+        SDL.SDL_Event evento;
+        open = true;
+        
+
+        while(open){
+            SDL.SDL_PollEvent(out evento);
+            if(evento.type == SDL.SDL_EventType.SDL_QUIT){
+                open = false;
+            }
+            if(evento.type == SDL.SDL_EventType.SDL_KEYDOWN){
+                switch (evento.key.keysym.sym){
+                    case SDL.SDL_Keycode.SDLK_1:
+                        cpu.Keyboard = 1;
+                        Console.WriteLine(cpu.Keyboard);
+                        break;
+
+                }
+            }
+        }
+    }
+}
