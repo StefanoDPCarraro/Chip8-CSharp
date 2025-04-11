5a2bf5063af1225b1bb456815fa7b5ee574a5702
Stefano <stefanodpc29@gmail.com>
2025-04-11 18:03:16 -0300

    Listening and handling for all the supported keys in the chip8 system

--- Chip8/Program.cs
+++ Chip8/Program.cs
@@ -23,10 +23,84 @@ public class Program{
             if(evento.type == SDL.SDL_EventType.SDL_KEYDOWN){
                 switch (evento.key.keysym.sym){
                     case SDL.SDL_Keycode.SDLK_1:
-                        cpu.Keyboard = 1;
+                        cpu.Keyboard = 0x01;
+                        Console.WriteLine(cpu.Keyboard);
+                        break;
+                    
+                    case SDL.SDL_Keycode.SDLK_2:
+                        cpu.Keyboard = 0x02;
                         Console.WriteLine(cpu.Keyboard);
                         break;
 
+                    case SDL.SDL_Keycode.SDLK_3:
+                        cpu.Keyboard = 0x03;
+                        Console.WriteLine(cpu.Keyboard);
+                        break;
+
+                    case SDL.SDL_Keycode.SDLK_4:
+                        cpu.Keyboard = 0x0C;
+                        Console.WriteLine(cpu.Keyboard);
+                        break;
+
+                    case SDL.SDL_Keycode.SDLK_q:
+                        cpu.Keyboard = 0x04;
+                        Console.WriteLine(cpu.Keyboard);
+                        break;
+
+                    case SDL.SDL_Keycode.SDLK_w:
+                        cpu.Keyboard = 0x05;
+                        Console.WriteLine(cpu.Keyboard);
+                        break;
+
+                    case SDL.SDL_Keycode.SDLK_e:
+                        cpu.Keyboard = 0x06;
+                        Console.WriteLine(cpu.Keyboard);
+                        break;
+
+                    case SDL.SDL_Keycode.SDLK_r:
+                        cpu.Keyboard = 0x0D;
+                        Console.WriteLine(cpu.Keyboard);
+                        break;
+
+                    case SDL.SDL_Keycode.SDLK_a:
+                        cpu.Keyboard = 0x07;
+                        Console.WriteLine(cpu.Keyboard);
+                        break;
+
+                    case SDL.SDL_Keycode.SDLK_s:
+                        cpu.Keyboard = 0x08;
+                        Console.WriteLine(cpu.Keyboard);
+                        break;
+
+                    case SDL.SDL_Keycode.SDLK_d:
+                        cpu.Keyboard = 0x09;
+                        Console.WriteLine(cpu.Keyboard);
+                        break;
+
+                    case SDL.SDL_Keycode.SDLK_f:
+                        cpu.Keyboard = 0x0E;
+                        Console.WriteLine(cpu.Keyboard);
+                        break;
+
+                    case SDL.SDL_Keycode.SDLK_z:
+                        cpu.Keyboard = 0x0A;
+                        Console.WriteLine(cpu.Keyboard);
+                        break;
+
+                    case SDL.SDL_Keycode.SDLK_x:
+                        cpu.Keyboard = 0x00;
+                        Console.WriteLine(cpu.Keyboard);
+                        break;
+
+                    case SDL.SDL_Keycode.SDLK_c:
+                        cpu.Keyboard = 0x0B;
+                        Console.WriteLine(cpu.Keyboard);
+                        break;
+
+                    case SDL.SDL_Keycode.SDLK_v:
+                        cpu.Keyboard = 0x0F;
+                        Console.WriteLine(cpu.Keyboard);
+                        break;
                 }
             }
         }
